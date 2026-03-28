861f89e841147d0d5e064f453302290864c69894
Guilherme Micelli <gmnetto@gmail.com>
2026-03-28 20:40:49 -0300

    feat: bootstrap android wrapper with capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ai.guinas.oneMoreOrbit',
+  appName: 'One More Orbit',
+  webDir: 'dist'
+};
+
+export default config;
